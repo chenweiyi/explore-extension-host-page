4b7ab23d3bd42c39cdb3fe5d6024e9eb65923386
chenweiyi <737649321@qq.com>
2023-09-11 09:03:25 +0800

    feat: improve new tab

--- README.md
+++ README.md
@@ -8,7 +8,6 @@
 ![GitHub action badge](https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/actions/workflows/build-zip.yml/badge.svg)
 <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://github.com/Jonghakseo/chrome-extension-boilerplate-react-viteFactions&count_bg=%23#222222&title_bg=%23#454545&title=😀&edge_flat=true" alt="hits"/>
 
-
 > This project is listed in the [Awesome Vite](https://github.com/vitejs/awesome-vite)
 
 </div>
@@ -21,15 +20,18 @@
   - [Procedures](#procedures)
 - [Screenshots](#screenshots)
   - [NewTab](#newtab)
-  - [Popup](#popup)  
+  - [Popup](#popup)
 - [Examples](#examples)
 - [Documents](#documents)
 
 ## Intro <a name="intro"></a>
+
 This boilerplate is made for creating chrome extensions using React and Typescript.
+
 > The focus was on improving the build speed and development experience with Vite.
 
 ## Features <a name="features"></a>
+
 - [React 18](https://reactjs.org/)
 - [TypeScript](https://www.typescriptlang.org/)
 - [Jest](https://jestjs.io/)
@@ -45,8 +47,9 @@ This boilerplate is made for creating chrome extensions using React and Typescri
 ## Installation <a name="installation"></a>
 
 ### Procedures <a name="procedures"></a>
+
 1. Clone this repository.
-2. Change `name` and `description` in package.json => **Auto synchronize with manifest** 
+2. Change `name` and `description` in package.json => **Auto synchronize with manifest**
 3. Run `yarn install` or `npm i` (check your node version >= 16.6, recommended >= 18)
 4. Run `yarn dev` or `npm run dev`
 5. Load Extension on Chrome
@@ -68,30 +71,29 @@ This boilerplate is made for creating chrome extensions using React and Typescri
 <img width="314" alt="popup" src="https://user-images.githubusercontent.com/53500778/203561728-23517d46-12e3-4139-8a4f-e0b2f22a6ab3.png">
 
 ## Examples <a name="examples"></a>
+
 - https://github.com/Jonghakseo/drag-gpt-extension
 - https://github.com/Jonghakseo/pr-commit-noti
 - https://github.com/ariburaco/chatgpt-file-uploader-extended
 
 ## Documents <a name="documents"></a>
+
 - [Vite Plugin](https://vitejs.dev/guide/api-plugin.html)
 - [ChromeExtension](https://developer.chrome.com/docs/extensions/mv3/)
 - [Rollup](https://rollupjs.org/guide/en/)
 - [Rollup-plugin-chrome-extension](https://www.extend-chrome.dev/rollup-plugin)
 
-
 ## Star History
 
 [![Star History Chart](https://api.star-history.com/svg?repos=Jonghakseo/chrome-extension-boilerplate-react-vite&type=Date)](https://star-history.com/#Jonghakseo/chrome-extension-boilerplate-react-vite&Date)
 
-
-
 ---
+
 ## Thanks To
 
-| [Jetbrains](https://jb.gg/OpenSourceSupport)                                                                           | [Jackson Hong](https://www.linkedin.com/in/j-acks0n/)                                            |
-|--------------------------------------------------------------------------------------------------------------------------------------------|--------------------------------------------------------------------------------------------------|
+| [Jetbrains](https://jb.gg/OpenSourceSupport)                                                                                               | [Jackson Hong](https://www.linkedin.com/in/j-acks0n/)                                            |
+| ------------------------------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------ |
 | <img width="100" src="https://resources.jetbrains.com/storage/products/company/brand/logos/jb_beam.png" alt="JetBrains Logo (Main) logo."> | <img width="100" src='https://avatars.githubusercontent.com/u/23139754?v=4' alt='Jackson Hong'/> |
-
 
 ---
 
--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,44 +1,200 @@
-import React from "react";
-import logo from "@assets/img/logo.svg";
-import "@pages/newtab/Newtab.css";
-import "@pages/newtab/Newtab.scss";
-import useStorage from "@src/shared/hooks/useStorage";
-import exampleThemeStorage from "@src/shared/storages/exampleThemeStorage";
-import withSuspense from "@src/shared/hoc/withSuspense";
+import React, { useEffect, useState } from 'react'
+import '@pages/newtab/Newtab.css'
+import useStorage from '@src/shared/hooks/useStorage'
+import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage'
+import withSuspense from '@src/shared/hoc/withSuspense'
+import { isNil } from 'lodash-es'
+
+interface ICategoryKey extends chrome.bookmarks.BookmarkTreeNode {
+  parentIds?: Set<string>
+  parentTitles?: string[]
+}
+
+type ICategoryData = Map<ICategoryKey, chrome.bookmarks.BookmarkTreeNode[]>
 
 const Newtab = () => {
-  const theme = useStorage(exampleThemeStorage);
+  // const theme = useStorage(exampleThemeStorage)
+
+  const [allData, setAllData] = useState<chrome.bookmarks.BookmarkTreeNode[]>(
+    []
+  )
+  const [categoryData, setCategoryData] = useState<ICategoryData>(new Map())
+  const [folders, setFolders] = useState<Array<ICategoryKey>>([])
+
+  const res: chrome.bookmarks.BookmarkTreeNode[] = []
+  const mapRes: ICategoryData = new Map()
+
+  function processBookmarkNodes(nodes: chrome.bookmarks.BookmarkTreeNode[]) {
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i]
+      if (node.url) {
+        // 处理书签节点的URL
+        // console.log(node.url)
+        res.push(node)
+      }
+      if (node.children) {
+        // 递归处理子节点
+        processBookmarkNodes(node.children)
+      }
+    }
+  }
+
+  function processBookmarkNodes2(
+    nodes: ICategoryKey[],
+    parentNode?: ICategoryKey
+  ) {
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i]
+      if (isNil(node.url)) {
+        !node.parentIds && (node.parentIds = new Set())
+        node.parentIds.add(node.parentId)
+        !node.parentTitles && (node.parentTitles = [])
+        parentNode && node.parentTitles.push(parentNode.title)
+      }
+      if (node.children) {
+        if (node.children.find((node) => node.url)) {
+          mapRes.set(
+            node,
+            node.children.filter((node) => !node.children && node.url)
+          )
+        }
+        // 递归处理子节点
+        processBookmarkNodes2(node.children, node)
+      }
+    }
+  }
+
+  function traverseAllData() {
+    chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
+      console.log(bookmarkTreeNodes)
+      processBookmarkNodes(bookmarkTreeNodes)
+      setAllData(res)
+    })
+  }
+
+  function traverseCategoryData() {
+    chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
+      processBookmarkNodes2(bookmarkTreeNodes)
+      const newMapRes: ICategoryData = new Map()
+      newMapRes.set(
+        {
+          id: '-1',
+          title: '全部'
+        },
+        res
+      )
+      for (const [key, val] of mapRes) {
+        newMapRes.set(key, val)
+      }
+      setCategoryData(newMapRes)
+    })
+  }
+
+  useEffect(() => {
+    traverseAllData()
+    traverseCategoryData()
+  }, [])
+
+  useEffect(() => {
+    const folders = []
+    for (const key of categoryData.keys()) {
+      folders.push(key)
+    }
+    setFolders(folders)
+  }, [categoryData.size])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-        <span className="text-lime-400">
-          The color of this paragraph is defined using Tailwind CSS.
-        </span>
-        <button
-          style={{
-            color: theme === "light" ? "#fff" : "#000",
-          }}
-          onClick={exampleThemeStorage.toggle}
-        >
-          Toggle theme: [{theme}]
-        </button>
-      </header>
+    <div
+      className='
+        app 
+        p-[16px] 
+        bg-slate-500 
+        w-full 
+        min-h-full
+        flex
+        flex-col
+        items-stretch
+      '
+    >
+      <div className='filter-area flex mb-[24px]'>
+        <div className='filter-title mr-[8px] text-white whitespace-nowrap'>
+          文件夹：
+        </div>
+        <div className='filter-content flex flex-wrap'>
+          {folders.map((folder) => {
+            return (
+              <span
+                key={folder.id}
+                className='
+                  folder-item
+                  bg-gray-300
+                  rounded-[2px]
+                  text-black
+                  p-[2px]
+                  mr-[8px]
+                  mb-[8px]
+                '
+              >
+                {folder.title}
+              </span>
+            )
+          })}
+        </div>
+      </div>
+      <div
+        className='
+          app-content 
+          flex
+          flex-wrap
+          justify-around
+        '
+      >
+        {allData.map((d) => {
+          return (
+            <div
+              className='
+                item 
+                w-[160px] 
+                h-[50px] 
+                mr-[12px] 
+                mb-[16px] 
+                border 
+                border-cyan-300
+                px-[8px] 
+                py-[12px] 
+                rounded-[4px] 
+                bg-cyan-300
+                overflow-hidden 
+                text-ellipsis 
+                whitespace-nowrap
+                text-black
+                font-medium
+                cursor-pointer
+                shadow-normal
+                hover:font-bold
+                hover:bg-cyan-500
+                hover:shadow-active
+                hover:text-white
+              '
+              key={d.id}
+              title={d.title}
+            >
+              <a
+                href={d.url}
+                target='_blank'
+                rel='noreferrer'
+                className='
+                  
+                '
+              >
+                {d.title}
+              </a>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default withSuspense(Newtab);
+export default withSuspense(Newtab)
