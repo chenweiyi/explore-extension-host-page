01161c70d7c9f2cb6fc9da55c23e9d45a667badf
chenweiyi <737649321@qq.com>
2024-07-11 14:05:43 +0800

    feat: 国际化字段添加

--- src/pages/newtab/Calendar.tsx
+++ src/pages/newtab/Calendar.tsx
@@ -1,6 +1,6 @@
 import withSuspense from '@src/shared/hoc/withSuspense'
 const Calendar = () => {
-  return <div>Calendar</div>
+  return <div>{t('common_placeholder')}</div>
 }
 
 export default withSuspense(Calendar)
